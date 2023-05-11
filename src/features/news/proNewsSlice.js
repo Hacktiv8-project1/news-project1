59d22583cfe449b18e4ab04ff45fd8d157ed2da9
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2023-05-11 19:12:19 +0700

    test deploy get api

--- src/features/news/proNewsSlice.js
+++ src/features/news/proNewsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const PRO_URL = `https://newsapi.org/v2/everything?q=Progaming&from=2023-04-11&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`;
+const PRO_URL = `https://newsapi.org/v2/everything?q=Progaming&from=2023-04-11&sortBy=popularity&apiKey=16d7589cf0574ceb98d7827cebba4d32`;
 
 const initialState = {
   proNews: [],
