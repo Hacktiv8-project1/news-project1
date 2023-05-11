a14485268d88fb4e95c9ffdfc2ca5029bb56b6c2
ddnamn <notimportantfile931900@gmail.com>
2023-05-11 10:59:25 +0700

    dot env added

--- src/features/news/covNewsSlice.js
+++ src/features/news/covNewsSlice.js
@@ -1,17 +1,19 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const COV_URL =
-  "https://newsapi.org/v2/everything?q=Covid&from=2023-04-11&sortBy=popularity&apiKey=16d7589cf0574ceb98d7827cebba4d32";
+const COV_URL = `https://newsapi.org/v2/everything?q=Covid&from=2023-04-11&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`;
 
 const initialState = {
   covNews: [],
 };
 
-export const fetchCovNews = createAsyncThunk("covNews/fetchCovNews", async () => {
-  const response = await axios.get(COV_URL);
-  return response.data.articles;
-});
+export const fetchCovNews = createAsyncThunk(
+  "covNews/fetchCovNews",
+  async () => {
+    const response = await axios.get(COV_URL);
+    return response.data.articles;
+  }
+);
 
 export const covNewsSlice = createSlice({
   name: "covNews",
